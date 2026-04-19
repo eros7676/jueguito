851d5f5cfeb3d83f127a58b36e3185884e15699a
eros76 <erosrafael13@gmail.com>
2026-04-19 15:09:23 -0300

    added throw function and wasd input. fixed ggun bugs and physics

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
@@ -10,25 +11,22 @@ public class Movement : MonoBehaviour
     public InputActionReference move;
     public float speed;
 
-    public float k = 10f;
     public float distanceGrab = 3f;
+    public float gVel = 30f;
 
     private Vector2 moveDirection;
     private Vector2 mousePosition;
-    private bool mousePressing;
+    private bool LMousePressing, RMousePressing, ignoreRMB;
     private Rigidbody2D rb2;
-    private Transform containerTransform;
-    private Collider2D triangleCollider;
-
-    private bool isGrabbing;
+    private Transform container, grabbingBox, gPoint;
+    private Rigidbody2D grabbingBoxRB;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("This is a test!");
         rb2 = GetComponent<Rigidbody2D>();
-        containerTransform = transform.GetChild(0);
-        triangleCollider = GetComponent<PolygonCollider2D>();
+        container = transform.GetChild(0);
+        gPoint = transform.GetChild(0).GetChild(0);
     }
 
     // Update is called once per frame
@@ -38,7 +36,10 @@ public class Movement : MonoBehaviour
 
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        mousePressing = Input.GetMouseButton(0);
+        LMousePressing = Input.GetMouseButton(0);
+        RMousePressing = Input.GetMouseButton(1);
+
+        if (!RMousePressing) ignoreRMB = false;
     }
 
     void FixedUpdate()
@@ -47,63 +48,58 @@ public class Movement : MonoBehaviour
         rb2.linearVelocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
 
         // get mouse position & normalize
-        Vector2 dir = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);        
-
-        dir = normalizeVector(dir, k);
+        Vector3 dir= mousePosition - (Vector2)transform.position;
+        Vector3 dirN = dir.normalized;        
 
         // rotate container
-        containerTransform.LookAt(transform.position + (Vector3) dir);
+        container.LookAt(transform.position + dirN);
 
         // raycast
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir);
-        Debug.DrawRay(transform.position, dir, Color.red);
-
-        // apply force to hitted object
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dirN);
+        Debug.DrawRay(transform.position, dirN * dir.magnitude, Color.red);
         Rigidbody2D rbhit;
 
-        if (!mousePressing) isGrabbing = false;
 
-        if (hit && hit.transform.TryGetComponent<Rigidbody2D>(out rbhit) && mousePressing)
+        
+
+
+
+
+        // Apply Force to Box
+        if (grabbingBox != null)
+        {
+            Vector2 delta = gPoint.position - grabbingBox.position;
+
+            grabbingBoxRB.linearVelocity = delta.normalized * delta.magnitude / 0.1f;
+
+            if (LMousePressing)
+            {
+                // grabbingBoxRB.AddForce(dirN * gVel * 100);
+                grabbingBoxRB.linearVelocity = dirN * gVel;
+                
+                grabbingBox = null;
+                ignoreRMB = true;
+            }
+        }
+
+
+        // Stop Grabbing
+        if (!RMousePressing) grabbingBox = null;
+
+        else if (!ignoreRMB && grabbingBox == null && hit && hit.transform.TryGetComponent<Rigidbody2D>(out rbhit))
         {
 
             if (hit.distance >= distanceGrab)
             {
-                rbhit.AddForce(-dir);
+                rbhit.AddForce(-dirN * gVel);
             }
-            else if (!isGrabbing)
+            else
             {
-                // rbhit.gravityScale = 0;
-                rbhit.bodyType = RigidbodyType2D.Kinematic;
-                hit.transform.SetParent(transform.GetChild(0).GetChild(0));
-                hit.transform.localPosition = Vector3.zero;
-
-                isGrabbing = true;
+                grabbingBox = hit.transform;
+                grabbingBoxRB = rbhit;
             }
-
-            
         }
         
 
-    }
-
-    // void OnTriggerEnter2D(Collider2D collision)
-    // {
-    //     if(!collision.CompareTag("Movable")) return;
-
-    //     flagGrabbing = true;
-    // }
-
-    // void OnTriggerExit2D(Collider2D collision)
-    // {
-    //     if(!collision.CompareTag("Movable")) return;
-
-    //     flagGrabbing = false;
-    // }
-
-    static Vector2 normalizeVector(Vector2 v, float k)
-    {
-        float aux = (float) (k / Math.Sqrt(Math.Pow(v.x, 2) + Math.Pow(v.y, 2)));
-
-        return new Vector2(v.x * aux, v.y * aux);
     }
 }
